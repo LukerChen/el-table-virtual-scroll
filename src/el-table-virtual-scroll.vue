083e800d9cf9dde22ea199557751e122fcd37fd6
Luker <service@pcboy.me>
2025-12-04 11:49:33 +0800

    解决列超过14个横向滚动时错位表头错位
    update

--- src/el-table-virtual-scroll.vue
+++ src/el-table-virtual-scroll.vue
@@ -276,6 +276,13 @@ export default {
       if (!this.scroller) return
       this.triggleScroll = true
 
+      //横向滚动不需要处理事件
+      if (typeof shouldUpdate === 'object' && shouldUpdate.type === 'scroll' && this.scroller.scrollTop === this.scrollPos[0]) {
+        return
+      }
+      //解决scroll事件死循环的问题
+      if (typeof shouldUpdate === 'boolean' && shouldUpdate === false) return
+
       // 【修复】如果使用v-show 进行切换表格会特别卡顿 #30；
       // 【原因】v-show为false时，表格内滚动容器的高度为auto，没有滚动条限制，虚拟滚动计算渲染全部内容
       if (this.isInnerScroll && !this.scroller.style.height && !this.scroller.style.maxHeight) return
